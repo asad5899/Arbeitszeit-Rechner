cdd8762629d88b5c5e570cc1941ceb9793705a8e
asad5899 <asad5899a@gmail.com>
2026-07-16 03:35:46 -0700

    1

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -221,16 +221,18 @@ ${xDefault}
     }
   }
 
+  // Clean trailing spaces and tabs from urlNodes, but keep leading spaces of the first line
+  const cleanUrlNodes = urlNodes.replace(/[ \t]+$/gm, '');
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
         xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"
         xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
-${urlNodes.trim()}
-</urlset>`;
+${cleanUrlNodes}</urlset>`;
 
-  return new Response(xml.trim(), {
+  return new Response(xml, {
     headers: {
       'Content-Type': 'application/xml',
       'X-Content-Type-Options': 'nosniff',
